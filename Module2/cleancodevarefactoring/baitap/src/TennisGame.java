d6eef87af84cb794114c91c0df0f88ad04c4ac38
HoangAchi <hoangphoniex@gmail.com>
2019-06-13 09:00:00 +0700

    cleancodevarefactoring

--- Module2/cleancodevarefactoring/baitap/src/TennisGame.java
+++ Module2/cleancodevarefactoring/baitap/src/TennisGame.java
@@ -1,4 +1,73 @@
-package PACKAGE_NAME;
-
 public class TennisGame {
+    public static final int ZEROSCORE=0;
+    public static final int ONESCORE=1;
+    public static final int TWOSCORE=2;
+    public static final int THREESCORE=3;
+    public static final int FOURSCORE=4;
+    public static String getScore(String playerone, String playertwo, int scoreplayerone, int scoreplayertwo) {
+        String result = "";
+        int tempScore;
+        if (scoreplayerone==scoreplayertwo)
+        {
+            switch (scoreplayerone)
+            {
+                case ZEROSCORE:
+                    result = "Love-All";
+                    break;
+                case ONESCORE:
+                    result = "Fifteen-All";
+                    break;
+                case TWOSCORE:
+                    result = "Thirty-All";
+                    break;
+                case THREESCORE:
+                    result = "Forty-All";
+                    break;
+                default:
+                    result = "Deuce";
+                    break;
+
+            }
+        }
+        else if (scoreplayerone>=FOURSCORE || scoreplayertwo>=FOURSCORE)
+        {
+            result = getString(scoreplayerone, scoreplayertwo);
+        }
+        else
+        {
+            for (int i=1; i<3; i++)
+            {
+                if (i==1) tempScore = scoreplayerone;
+                else { result+="-"; tempScore = scoreplayertwo;}
+                switch(tempScore)
+                {
+                    case ZEROSCORE:
+                        result+="Love";
+                        break;
+                    case ONESCORE:
+                        result+="Fifteen";
+                        break;
+                    case TWOSCORE:
+                        result+="Thirty";
+                        break;
+                    case THREESCORE:
+                        result+="Forty";
+                        break;
+                }
+            }
+        }
+
+        System.out.println(result);
+        return result;
+    }
+
+    private static String getString(int scoreplayerone, int scoreplayertwo) {
+        String result;
+        int minusResult = scoreplayerone-scoreplayertwo;
+        if (minusResult==ONESCORE) result ="Advantage player1";
+        else if (minusResult ==-ONESCORE) result ="Advantage player2";
+        else if (minusResult>=TWOSCORE) result = "Win for player1";
+        else result ="Win for player2";
+        return result;
+    }
 }
--- Module2/cleancodevarefactoring/thuchanh/src/calculator/CalcutorTest.java
+++ Module2/cleancodevarefactoring/thuchanh/src/calculator/CalcutorTest.java
@@ -1,4 +1,83 @@
 package calculator;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class CalcutorTest {
+    @Test
+    @DisplayName("Testing addition")
+    void testCalculateAdd() {
+        int firstOperand = 1;
+        int secondOperand = 1;
+        char operator = '+';
+        int expected = 2;
+
+        int result = Calculator.calculate(firstOperand, secondOperand, operator);
+        assertEquals(expected, result);
+        System.out.println("CalculateAdd " + result);
+    }
+
+    @Test
+    @DisplayName("Testing subtraction")
+    void testCalculateSub() {
+        int firstOperand = 2;
+        int secondOperand = 1;
+        char operator = '-';
+        int expected = 1;
+
+        int result = Calculator.calculate(firstOperand, secondOperand, operator);
+        assertEquals(expected, result);
+        System.out.println("CalculateSub " + result);
+    }
+
+    @Test
+    @DisplayName("Testing multiplication")
+    void testCalculateMul() {
+        int firstOperand = 2;
+        int secondOperand = 2;
+        char operator = '*';
+        int expected = 4;
+
+        int result = Calculator.calculate(firstOperand, secondOperand, operator);
+        assertEquals(expected, result);
+        System.out.println("CalculateMul " + result);
+    }
+
+    @Test
+    @DisplayName("Testing division")
+    void testCalculateDiv() {
+        int firstOperand = 6;
+        int secondOperand = 3;
+        char operator = '/';
+        int expected = 2;
+
+        int result = Calculator.calculate(firstOperand, secondOperand, operator);
+        assertEquals(expected, result);
+        System.out.println("CalculateDiv " + result);
+    }
+
+    @Test
+    @DisplayName("Testing division by zero")
+    void testCalculateDivByZero() {
+        int firstOperand = 2;
+        int secondOperand = 0;
+        char operator = '/';
+
+
+        System.out.println(assertThrows(RuntimeException.class,
+                ()-> {Calculator.calculate(firstOperand, secondOperand, operator);}));
+    }
+
+    @Test
+    @DisplayName("Testing wrong operator")
+    void testCalculateWrongOperator() {
+        int firstOperand = 2;
+        int secondOperand = 0;
+        char operator = '=';
+
+        System.out.println(assertThrows(RuntimeException.class,
+                ()-> {Calculator.calculate(firstOperand, secondOperand, operator);}));
+    }
 }
